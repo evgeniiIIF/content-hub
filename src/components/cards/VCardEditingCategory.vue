b3de785040d67e6402f92d1a8c79ea4b70ce3b75
pro100 <pro100.sto@yandex.ru>
2023-05-25 17:59:48 +0300

    add card edit

--- src/components/cards/VCardEditingCategory.vue
+++ src/components/cards/VCardEditingCategory.vue
@@ -35,30 +35,32 @@
         </li>
       </ul>
     </div>
-    <ul class="card-editing-category__hints">
-      <li class="card-editing-category__hint">
-        <div class="hint-card-editing-category">
-          <div class="card-editing-category__icon">
-            <img
-              src="@/assets/img/static/decorative-icon/Info.svg"
-              alt="info"
-            />
+    <div class="card-editing-category__hints">
+      <ul class="card-editing-category__hints-body">
+        <li class="card-editing-category__hint">
+          <div class="hint-card-editing-category">
+            <div class="hint-card-editing-category__icon">
+              <img
+                src="@/assets/img/static/decorative-icon/Info.svg"
+                alt="info"
+              />
+            </div>
+            <div class="hint-card-editing-category__text">Если еще не назначена(ы) категори МП. Сопоставте категории МП с локальной (из выподающего списка).</div>
           </div>
-          <div class="card-editing-category__text">Если еще не назначена(ы) категори МП. Сопоставте категории МП с локальной (из выподающего списка).</div>
-        </div>
-      </li>
-      <li class="card-editing-category__hint">
-        <div class="hint-card-editing-category">
-          <div class="card-editing-category__icon">
-            <img
-              src="@/assets/img/static/decorative-icon/Info.svg"
-              alt="info"
-            />
+        </li>
+        <li class="hint-card-editing-category__hint">
+          <div class="hint-card-editing-category">
+            <div class="hint-card-editing-category__icon">
+              <img
+                src="@/assets/img/static/decorative-icon/Info.svg"
+                alt="info"
+              />
+            </div>
+            <div class="hint-card-editing-category__text">Если еще не назначена(ы) категори МП. Сопоставте категории МП с локальной (из выподающего списка).</div>
           </div>
-          <div class="card-editing-category__text">Если еще не назначена(ы) категори МП. Сопоставте категории МП с локальной (из выподающего списка).</div>
-        </div>
-      </li>
-    </ul>
+        </li>
+      </ul>
+    </div>
   </section>
 </template>
 
@@ -91,27 +93,38 @@
 
 <style lang="scss">
   @import '@/assets/scss/smart-grid.scss';
+  @import '@/assets/scss/mixins.scss';
 
   .card-editing-category {
     background: #ffffff;
     border-radius: 4px;
+
     &__content {
-      padding: 20px;
       margin-bottom: auto;
     }
     &__top {
+      padding: 20px;
       display: flex;
       align-items: center;
+      border-bottom: 1px solid #ebedf1;
     }
 
     &__button-back {
+      margin-right: 34px;
       .button {
+        padding: 8px 14px;
         background: transparent;
         border: 1px solid #ebedf1;
       }
     }
 
     &__title {
+      font-family: 'Inter';
+      font-style: normal;
+      font-weight: 400;
+      font-size: 32px;
+      line-height: 125%;
+      color: #292929;
     }
 
     &__category-info {
@@ -121,6 +134,17 @@
     }
 
     &__hints {
+      padding: 4px;
+    }
+    &__hints-body {
+      padding: 16px;
+      & > *:first-child {
+        padding-bottom: 20px;
+        border-bottom: 1px solid #ebedf1;
+      }
+      & > *:last-child {
+        padding-top: 20px;
+      }
     }
 
     &__hint {
@@ -133,26 +157,97 @@
     }
   }
   .info-card-editing-category {
+    display: flex;
+    padding: 24px 20px;
+
     &__title {
+      margin-right: 20px;
+      font-family: 'Inter';
+      font-style: normal;
+      font-weight: 400;
+      font-size: 12px;
+      line-height: 200%;
+      color: #7e8d94;
     }
 
     &__name {
+      padding: 0px 8px;
+      background: #0077ff;
+      border-radius: 4px;
+      font-family: 'Inter';
+      font-style: normal;
+      font-weight: 400;
+      font-size: 14px;
+      line-height: 171%;
+      color: #ffffff;
     }
   }
   .tabs-card-editing-category {
+    padding: 0 20px;
+
+    & > * {
+      border-bottom: 1px solid #ebedf1;
+    }
+    & > *:first-child {
+      border-top: 1px solid #ebedf1;
+    }
+
     &__item {
+      padding: 4px 0;
+      border-radius: 4px;
+      cursor: pointer;
+
+      &:hover {
+        & .tab-card-editing-category {
+          background: #ebedf1;
+        }
+      }
     }
   }
   .tab-card-editing-category {
+    display: flex;
+    align-items: center;
+    padding: 8px 14px;
+
     &__step {
+      margin-right: 20px;
+      font-family: 'Inter';
+      font-style: normal;
+      font-weight: 600;
+      font-size: 12px;
+      line-height: 167%;
+      text-transform: uppercase;
+      color: #0077ff;
     }
 
     &__title {
+      margin-right: auto;
+      font-family: 'Inter';
+      font-style: normal;
+      font-weight: 400;
+      font-size: 14px;
+      line-height: 171%;
+      color: #292929;
     }
 
     &__arrow {
     }
   }
   .hint-card-editing-category {
+    display: flex;
+    align-items: center;
+
+    &__icon {
+      margin-right: 66px;
+    }
+    &__text {
+      max-width: 329px;
+      font-family: 'Inter';
+      font-style: normal;
+      font-weight: 400;
+      font-size: 12px;
+      line-height: 167%;
+      color: #292929;
+    }
   }
 </style>
